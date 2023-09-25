79278096058e4c7958a96b6e0ee94b805b79fa3e
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-25 14:53:55 +0100

    Handle NULL list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -49,7 +49,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *head, *ptr;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	head = (*list)->next;
