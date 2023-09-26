70d926a152e8d0f7be6a99b3e2f6167333c4662c
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-26 05:35:29 +0100

    Improve code

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -29,9 +29,12 @@ void shell_sort(int *array, size_t size)
 			/* Compare the current element with the element at index gap */
 			while (j >= gap && (array[j - gap] > temp))
 			{
+				/* Assign a greater value at this position */
 				array[j] = array[j - gap];
+				/* Go back, check if temp is lowest there */
 				j -= gap;
 			}
+			/* Insert temp in its correct position */
 			array[j] = temp;
 		}
 		print_array(array, size);
